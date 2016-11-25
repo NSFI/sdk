4b2fb8d0332ab5e84ed15633f133fab2447075aa
方波 <bluepooh@126.com>
2016-11-25 13:27:04 +0800

    IE plugin support

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports =  {
 				loader: 'babel',
 				query: {
 					presets: ['es2015-loose', 'stage-0'],
-					plugins: ['transform-runtime']
+					plugins: ['transform-runtime', 'transform-es3-property-literals','transform-es3-member-expression-literals']
 				}
 			}
         ]
