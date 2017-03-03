70ed5cb9586d7e72967654f9435b38837642180f
jianfulee <jianfulee@hotmail.com>
2017-03-03 17:56:14 +0800

    add some util function

--- src/util/util.js
+++ src/util/util.js
@@ -114,6 +114,24 @@ var each = function (o, func) {
 	}
 };
 
+/**
+ * merge
+ * @param o
+ * @return {void}
+ * @description 合并数据
+ */
+var merge = function (o) {
+    each(o, function (k, v) {
+        cache[k] = v;
+    });
+};
+
+/**
+ * serialize
+ * @param o
+ * @return {string}
+ * @description 序列化对象
+ */
 var serialize = function (o) {
 	var ret = [];
 	each(o, function (k, v) {
@@ -125,8 +143,172 @@ var serialize = function (o) {
 	return ret.join('&');
 };
 
+/**
+ * notification
+ *
+ * @returns {void}
+ */
 
-	
+var notification = function(){
+    var notify, timer;
+    return function(msg){
+        if(notify){
+            clearTimeout(timer);
+            notify.close();
+        }
+        if(window.Notification && window.Notification.permission !== "granted"){
+            Notification.requestPermission();
+        }
+
+        if (window.Notification && window.Notification.permission != "denied") {
+            notify = new Notification(msg.notify, {
+                tag: msg.tag,
+                body : msg.body,
+                icon : window.__YSFSDKADR__ + msg.icon
+            });
+            util.playAudio();
+            notify.onclick = function(){
+                notify&&notify.close();
+                window.focus();
+                ysf.openLayer();
+                ysf.NotifyMsgAndBubble({category : 'clearCircle'});
+
+            };
+
+            // close notify
+            timer = window.setTimeout(
+                function(){
+                    notify.close();
+                },20000
+            );
+        }
+    }
+};
+
+/**
+ * playAudio
+ *
+ * @returns {void}
+ */
+var playAudio = function(){
+    var audio = document.createElement('audio');
+    audio.src = window.__YSFSDKADR__ + "/prd/res/audio/message.mp3?26b875bad3e46bf6661b16a5d0080870";
+
+    return function(){
+        audio.play();
+    }
+};
+
+/**
+ * rand
+ *
+ * @returns {string}
+ */
+// 为没有uid表示的访客生成随机身份标识
+var rand = function (uid) {
+    if (!!uid) {
+        return 'ysf-' + uid;
+    }
+    var ret = [];
+    for (var i = 0, n; i < 20; ++i) {
+        n = Math.floor(Math.random() * chars.length);
+        ret.push(chars.charAt(n));
+    }
+    return ret.join('').toLowerCase();
+};
+
+/**
+ * migrate
+ *
+ * @returns {void}
+ */
+var migrate = function () {
+    // migrate uid
+    var uid;
+    if (/YSF_UID\s*=\s*(.*?)(?=;|$)/i.test(document.cookie)) {
+        uid = RegExp.$1;
+    }
+    if (!!uid) {
+        localStorage.setItem('YSF_UID', uid);
+    }
+    // migrate last uid
+    var uid;
+    if (/YSF_LAST\s*=\s*(.*?)(?=;|$)/i.test(document.cookie)) {
+        uid = RegExp.$1;
+    }
+    if (!!uid) {
+        localStorage.setItem('YSF_LAST', uid);
+    }
+    // remove cookie
+    var expires = (new Date(1990, 11, 30)).toGMTString();
+    document.cookie = 'YSF_UID=;path=/;expires=' + expires;
+    document.cookie = 'YSF_LAST=;path=/;expires=' + expires;
+};
+
+/**
+ * refresh
+ * @param {uid}
+ * @returns {void}
+ * @description 同步 YSF-[appkey]-UID 信息,访客端轮询改动
+ */
+var refresh = function (uid) {
+    uid = uid || '';
+    var dvc = device(),
+        lst = lastUID();
+    // check device
+
+    // 1. uid != '' && lst == '' anonymous -> realname no update;
+    // 2. uid == '' && lst != '' realname -> anonymouse update
+    // re-device if
+    // - first time
+    // - user switch
+    if(!dvc || (uid == '' && lst != '')) {
+        // dvc change then clear localStorage
+        // dvc = rand(uid);
+        dvc = uid || dvc || rand(uid);
+        sendMsg('synckey:'+dvc);
+
+    }
+
+    cache.device = dvc;
+    localStorage.setItem('YSF-' + cache['appKey'].toUpperCase()+'-UID', uid || dvc);
+    localStorage.setItem('YSF-' + cache['appKey'].toUpperCase()+ '-LAST', uid || '');
+};
+
+/**
+ * device
+ * @param {void}
+ * @returns {string}
+ * @description  从LocalStorage中获取设备Id
+ */
+var device = function () {
+    return localStorage.getItem('YSF-' + cache['appKey'].toUpperCase()+'-UID') || '';
+};
+
+/**
+ * lastUID
+ * @param {void}
+ * @returns {string}
+ * @description 从LocalStorage中获取 lastUId
+ */
+var lastUID = function () {
+    return localStorage.getItem('YSF-' + cache['appKey'].toUpperCase()+ '-LAST') || '';
+};
+
+/**
+ * updateDevice
+ * @param {void}
+ * @returns {void}
+ * @description 更新设备id
+ */
+var updateDevice = function(){
+    cache.device = rand();
+    localStorage.setItem('YSF-' + cache['appKey'].toUpperCase()+'-UID', cache.device);
+    sendMsg('synckey:'+ cache.device);
+};
+
+
+
 module.exports = {
 	mergeUrl : mergeUrl,
 	query2Object : query2Object,
@@ -135,5 +317,14 @@ module.exports = {
 	clearLocalItems : clearLocalItems,
 	random : random,
 	each : each,
-	serialize : serialize
+	serialize : serialize,
+    notification : notification,
+    playAudio : playAudio,
+    rand : rand,
+    migrate : migrate,
+    merge : merge,
+    refresh : refresh,
+    device : device,
+    lastUID : lastUID,
+    updateDevice : updateDevice
 }
